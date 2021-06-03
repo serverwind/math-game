e89e196d21e77ef5e8296d0ca13ec1ed855e326b
Alex <lestraeqq@gmail.com>
2021-06-03 10:32:08 +0300

    Update main.js
    В процессе создания функций

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
-let firstNum = Math.floor(Math.random() * 10) + 1; // генерация первого числа
-let secondNum = Math.floor(Math.random() * 10) + 1; // генерация второго числа
-let signArr = ['+', '-', '*', '/'];
+//HTML разметка
+const firstNumHtml = document.getElementById('first-num');
+const secondNumHtml = document.getElementById('second-num');
+const signHtml = document.getElementById('sign');
+const rightAnswerHtml = document.getElementById('right-answer');
+const userAnswerHtml = document.getElementById('user-answer');
+const sendAnswerHtml = document.getElementById('send-answer');
+sendAnswerHtml.addEventListener('click', checkAnswer);
 
-let sign = Math.floor(Math.random() * 4); // генерация символа
+function generateQuiz() {
+  let firstNum = Math.floor(Math.random() * 10) + 1; // генерация первого числа
+  let secondNum = Math.floor(Math.random() * 10) + 1; // генерация второго числа
+  let signArr = ['+', '-', '*', '/'];
 
-console.log(quiz) 
+  let sign = Math.floor(Math.random() * 4); // генерация символа
 
-if (signArr[sign] == '+') {
-	console.log(firstNum + secondNum);
-} 
-else if (signArr[sign] == '-') {
-	console.log(firstNum - secondNum);
+  return firstNum, secondNum, sign, signArr;
 }
-else if (signArr[sign] == '*') {
-	console.log(firstNum * secondNum);
-} else {
-	console.log(firstNum / secondNum);
+
+function renderHtml(signArr, firstNum, secondNum, sign) {
+	//Вывод примера в HTML
+  firstNumHtml.innerHTML = firstNum;
+  secondNumHtml.innerHTML = secondNum;
+  signHtml.innerHTML = signArr[sign];
+}
+
+function checkAnswer(signArr, firstNum, secondNum, sign) {
+  let rightAnswer;
+  console.log(firstNum, secondNum, sign)
+
+  if (signArr[sign] == '+') {
+    rightAnswer = firstNum + secondNum;
+  } else if (signArr[sign] == '-') {
+    rightAnswer = firstNum - secondNum;
+  } else if (signArr[sign] == '*') {
+    rightAnswer = firstNum * secondNum;
+  } else {
+    rightAnswer = firstNum / secondNum;
+  }
+
+  if (rightAnswer == userAnswerHtml.value) {
+    console.log('ye')
+  } else {
+    console.log('naw')
+  }
+  generateQuiz();
+  renderHtml();
 }
