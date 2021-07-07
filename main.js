fabea8b90bbfe8369fe109752e7200efe8eb5ca2
Alex <lestraeqq@gmail.com>
2021-07-07 09:43:32 +0300

    Update main.js
    Добавлена функция старта игры showGame()

--- main.js
+++ main.js
@@ -2,20 +2,38 @@ let myApp = {}; //один класс, в котором собраны пере
 let hearts = 3; //количество жизней (возможностей ошибиться) на начало игры
 
 //HTML разметка
+const startGameHtml = document.getElementById('start-game');
 const firstNumHtml = document.getElementById('first-num');
 const secondNumHtml = document.getElementById('second-num');
 const signHtml = document.getElementById('sign');
 const rightAnswerHtml = document.getElementById('right-answer');
 const userAnswerHtml = document.getElementById('user-answer');
 const sendAnswerHtml = document.getElementById('send-answer');
-const heartsAmount = document.getElementById('hearts-amount');
+const heartsHtml = document.querySelector('.hearts');
+const heartsAmountHtml = document.getElementById('hearts-amount');
+const mainDivHtml = document.getElementById('main');
+const answerHtml = document.querySelector('.answer');
+const gameOverHtml = document.getElementById('game-over');
 
+startGameHtml.addEventListener('click', showGame);
 sendAnswerHtml.addEventListener('click', checkAnswer);
 
+//Показываем все блоки игры и генерируем первый пример
+function showGame() {
+  startGameHtml.style.display = 'none';
+  heartsHtml.style.display = 'block';
+  mainDivHtml.style.display = 'flex';
+  answerHtml.style.display = 'block';
+
+  generateQuiz();
+  calcAnswer();
+}
+
 function generateQuiz() {
-	
-  if (hearts == 0) {
-  console.log('stop');
+  if (hearts == 0) { //Проверяем остались ли у игрока жизни. Если нет - закрываем игру
+    mainDivHtml.style.display = 'none';
+    answerHtml.style.display = 'none';
+    gameOverHtml.style.display = 'block';
   }
 
   myApp.firstNum = Math.floor(Math.random() * 10) + 1; // генерация первого числа
@@ -44,19 +62,19 @@ function calcAnswer() {
   } else {
     myApp.rightAnswer = myApp.firstNum / myApp.secondNum;
   }
+  console.log(myApp.rightAnswer)
 }
 
 //Функция проверки правильный ли юзер дал ответ
 function checkAnswer() {
   if (myApp.rightAnswer == userAnswerHtml.value) { //получаем его ответ
     console.log('ye');
-    hearts = 3;
   } else {
     console.log('naw');
     hearts -= 1;
   }
- 
+
   calcAnswer(); //Сравниваем
-  
-  heartsAmount.innerHTML = hearts;
+
+  heartsAmountHtml.innerHTML = hearts;
 }
